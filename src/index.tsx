018463a5a434f156300d16466547d4ce254d615d
davidcraig <davidcraig1986@gmail.com>
2025-12-30 18:40:16 +0000

    Update ts settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,3 @@
-export { default as Card } from "./Card";
-export { default as ExternalLink } from "./ExternalLink";
-export { default as Navbar } from "./Navbar";
+export { default as Card } from "./Card.tsx";
+export { default as ExternalLink } from "./ExternalLink.tsx";
+export { default as Navbar } from "./Navbar.tsx";
